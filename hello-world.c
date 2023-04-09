ae59db6464478ca37b9198863eb2c958ff3f32a3
MaeltonSI <msl54@aluno.ifal.edu.br>
2023-04-09 19:43:52 -0300

    Primeiro programa em C

---
+++ hello-world.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+Biblioteca stdio:
+https://docs.google.com/document/d/1_HovjWuPphZ6bd8KnQt6Cf7tQYFhugJtH7_VPSNCrGo/edit?usp=sharing
+*/
+
+int main(){
+    printf("Hello world!\n");
+    return 0;
+}
+/*
+Processo de compilação:
+https://drive.google.com/file/d/1IQVQxLS1llXNP6jqksiyDJPvlFffDEOH/view?usp=sharing
+*/
+
+/*
+Aqruivos .out:
+https://docs.google.com/document/d/15BCegj-Vl_uwrhYEeqaaFxdZH3ODHonqlt80HUX2weg/edit?usp=sharing
+*/
