93ba351208aeb61b4c8ff89bede47c6d8a3c4c30
MaeltonSI <msl54@aluno.ifal.edu.br>
2023-04-19 20:52:09 -0300

    iniciando operadores bit a bit

--- operadoresLogicos.c
+++ operadoresLogicos.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+void processar_opcoes(char o1, char o2, char o3, char o4, char o5, char o6, char o7, char o8,) {
+
+    if(o1) { printf("opcao 1 estah ativa\n"); }
+    if(o2) { printf("opcao 2 estah ativa\n"); }
+    if(o3) { printf("opcao 3 estah ativa\n"); }
+    if(o4) { printf("opcao 4 estah ativa\n"); }
+    if(o5) { printf("opcao 5 estah ativa\n"); }
+    if(o6) { printf("opcao 6 estah ativa\n"); }
+    if(o7) { printf("opcao 7 estah ativa\n"); }
+    if(o8) { printf("opcao 8 estah ativa\n"); }
+}
+
 int main() {
-
-    int a = 0;
-    int b = 1;
-
-    //&& (and), || (or), ! (not)
-    if (!a) {
-        printf("a é igual a 0, pois not 'a' é diferente de 0 e consequentemente verdadeiro. Por isso o escopo do if funcionou.\n");
-    };
-
+    char o1 = 1;
+    char o2 = 0;
+    char o3 = 1;
+    char o4 = 0;
+    char o5 = 1;
+    char o6 = 0;
+    char o7 = 0;
+    char o8 = 0;
+    
+    processar_opcoes(o1, o2, o3,o4, o5, o6, o7, o8);
     return 0;
 }
