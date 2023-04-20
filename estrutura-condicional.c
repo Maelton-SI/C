423cfa6a7f9f6d34194d34fd36f607601858da38
MaeltonSI <msl54@aluno.ifal.edu.br>
2023-04-19 21:55:25 -0300

    IF sem chaves

---
+++ estrutura-condicional.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 19;
+
+    //IFs sem chaves rodam apenas o primeiro comando.
+    if(idade<18)
+        printf("O amigo eh menor de idade"); printf("Estou fora do IF\n");
+}
