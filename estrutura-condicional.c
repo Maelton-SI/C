083e276f9d3f4604743f56fc590063fd54120022
MaeltonSI <msl54@aluno.ifal.edu.br>
2023-04-24 19:36:20 -0300

    Switch Case

--- estrutura-condicional.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int idade = 19;
-
-    //IFs sem chaves rodam apenas o primeiro comando.
-    if(idade<18)
-        printf("O amigo eh menor de idade"); printf("Estou fora do IF\n");
-}
