578b431cb1d5018f213a841691e4d125d60dd314
MaeltonSI <msl54@aluno.ifal.edu.br>
2023-04-26 21:10:24 -0300

    Segundo For loop e anotações

--- forLoop.c
+++ forLoop.c
@@ -2,7 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-    for (int qtdCarneiros = 1; qtdCarneiros<=10; qtdCarneiros++)
+    //for (;;){} é equivalente a:
+    //for (;1==1;){}, que sempre é verdade, ou seja, é um loop infinito
+
+    for (int qtdCarneiros = 1; qtdCarneiros>-1; qtdCarneiros--)
     {
         printf("%d caneirinhos\n", qtdCarneiros);
     }
