4ef9a35ec078fbf4287d94d2cdfa6d714b540dd0
MaeltonSI <msl54@aluno.ifal.edu.br>
2023-04-26 21:04:23 -0300

    Primeiro For loop

---
+++ forLoop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    for (int qtdCarneiros = 1; qtdCarneiros<=10; qtdCarneiros++)
+    {
+        printf("%d caneirinhos\n", qtdCarneiros);
+    }
+
+    return 0;
+}
